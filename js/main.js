bc9a2b265f09f304db79799d8fca4ca8c76cf53c
Joshua <44485177+joshuamatthewhagen@users.noreply.github.com>
2019-02-24 12:57:20 -0600

    yu

--- js/main.js
+++ js/main.js
@@ -81,8 +81,8 @@
     /*-- Smoth-Scroll --*/
     $('.mainmenu-area a[href*="#"]')
         // Remove links that don't actually link to anything
-        .not('[href="#"]')
-        .not('[href="#0"]')
+        //.not('[href="#"]')
+        //.not('[href="#0"]')
         .click(function (event) {
             // On-page links
             if (
