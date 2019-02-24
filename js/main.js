316b4c892308d9d99f99d4dc4a18abc72ab68670
Joshua <44485177+joshuamatthewhagen@users.noreply.github.com>
2019-02-24 13:15:42 -0600

    f

--- js/main.js
+++ js/main.js
@@ -79,16 +79,15 @@
     });
 
     /*-- Smoth-Scroll --*/
-    $('.mainmenu-area a[href*="#"]')
+    $('.mainmenu-area a[href*="location.href"]')
         // Remove links that don't actually link to anything
         .not('[href="#"]')
         .not('[href="#0"]')
         .click(function (event) {
             // On-page links
             if (
-                //location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
-               // location.hostname == this.hostname
-			   document.getElementById("demo").innerHTML = "location is " + window.location.href;
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+                location.hostname == this.hostname
             ) {
                 // Figure out element to scroll to
                 var target = $(this.hash);
