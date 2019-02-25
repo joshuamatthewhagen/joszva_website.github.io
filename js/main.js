d20857b030a96d477e9e639c59fa60cf730b9337
Joshua <44485177+joshuamatthewhagen@users.noreply.github.com>
2019-02-24 20:07:48 -0600

    f

--- js/main.js
+++ js/main.js
@@ -88,7 +88,7 @@
             if (
                 //location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
                 //location.hostname == this.hostname
-				window.location.assign(window.href);
+				document.url = document.location.href;
             ) {
                 // Figure out element to scroll to
                 var target = $(this.hash);
